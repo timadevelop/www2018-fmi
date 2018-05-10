59bbe91a48d35fce13677ed6bb0b8ce67e02fa2e
timadevelop <timadevelop@gmail.com>
2018-05-10 20:59:15 +0300

    hw js 1.1 & 1.2 + zipped

--- hw5.2/index.js
+++ hw5.2/index.js
@@ -1,14 +1,52 @@
 window.onload = init;
 
 function init() {
-  // DOM elements
+  //
+  // Get references to DOM elements
+  //
   var form = document.getElementById("registration-form");
   var loginInput = document.getElementById("login_input");
   var passwordInput = document.getElementById("password_input");
   var passwordConfirmationInput = document.getElementById("password_confirmation");
   var messageP = document.getElementById("message");
+  var submitButton = document.getElementById("submit-button");
 
-  // ui stuff
+  //
+  // Ajax stuff
+  //
+  function ajax(url, settings) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+      if (xhr.status == 200) {
+        settings.success(xhr.responseText);
+      } else {
+        console.error(xhr.responseText);
+      }
+    };
+    xhr.open(settings.method || 'GET', url, /* async */ true);
+    xhr.send(settings.data || null);
+  }
+  //
+  // ajax callback
+  //
+  var callback = function(text) {
+    // console.log(text);
+    showRegistered(JSON.parse(text));
+  };
+
+  function send_to_php(json) {
+    // console.log(json);
+    ajax('register.php', {
+      success: callback,
+      method: 'POST',
+      data: JSON.stringify(json)
+    });
+  }
+
+
+  //
+  // UI functions
+  //
   function showWarn(string) {
     messageP.classList.remove("success", "hide");
     messageP.classList.add("warn", "show");
@@ -26,7 +64,22 @@ function init() {
     messageP.classList.add("hide");
   }
 
-  // validators
+  function showRegistered(response) {
+    messageP.classList.remove("warn", "hide");
+    messageP.classList.add("success", "show");
+    submitButton.style.display = 'none';
+    var nodes = document.getElementsByTagName("input"); //.forEach(e => e.style.display = 'none');
+    for(i = 0; i < nodes.length; i++)
+    {
+      nodes[i].style.display = 'none';
+    }
+    messageP.innerHTML = "Hello, " + response.username + "!<br>Logged in successfully";
+  }
+
+
+  //
+  // Validators
+  //
   function isValidLogin(string) {
     return string.length >= 3 && string.length <= 10 && string.match("^[a-zA-Z0-9_]+$");
   }
@@ -35,17 +88,16 @@ function init() {
     return string.length >= 6 && string.match("[a-z]+") && string.match("[A-Z]+") && string.match("[0-9]+");
   }
 
-  // handlers
-  function submitHandler(event) {
-    if (checkLogin(event) &&
-      checkPassword(event) &&
-      checkPasswordConfirm(event)) {
-      showSuccess("Yes, send it!");
-      return true;
-    } else {
-      showWarn("Hmm.. there is some error in your form data...");
-      event.preventDefault();
+  //
+  // Check inputs
+  //
+  function checkLogin(event) {
+    if (!isValidLogin(loginInput.value)) {
+      showWarn("Login is not valid:<br>потребителско име - поне 3 символа и най-много 10 символа - букви, цифри и _");
       return false;
+    } else {
+      hideMsg();
+      return true;
     }
   }
 
@@ -78,18 +130,26 @@ function init() {
     }
   }
 
-
-  function checkLogin(event) {
-    if (!isValidLogin(loginInput.value)) {
-      showWarn("Login is not valid:<br>потребителско име - поне 3 символа и най-много 10 символа - букви, цифри и _");
+  // submit form
+  function submitHandler(event) {
+    if (checkLogin(event) && checkPassword(event) && checkPasswordConfirm(event)) {
+      showSuccess("Yes, send it!");
+      send_to_php({
+        "username": loginInput.value,
+        "password": passwordConfirmationInput.value
+      });
+      event.preventDefault();
       return false;
     } else {
-      hideMsg();
-      return true;
+      showWarn("Hmm.. there is some error in your form data...");
+      event.preventDefault();
+      return false;
     }
   }
 
-  // event listeners assigment
+  //
+  // Add event listeners
+  //
   form.addEventListener('submit', submitHandler);
   passwordConfirmationInput.addEventListener('input', checkPasswordConfirm);
   passwordInput.addEventListener('input', checkPassword);
