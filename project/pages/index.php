4a9d2ec0b051139b7283a20dbe72081eb7f74c54
timadevelop <timadevelop@gmail.com>
2018-06-20 22:47:35 +0300

    styles, init

--- project/pages/index.php
+++ project/pages/index.php
@@ -1,27 +1,52 @@
 <?php include_once("parts/header.php") ?>
-   <div class="flex space-between horizontal">
-      <a href="trips.html" class="bold">Back to Trips</a>
-      <a href="trips.html" class="bold">Remove this trip</a>
+<?php if (session_status() == PHP_SESSION_NONE) : ?>
+<div class="block">
+  <div class="container flex space-between horizontal">
+    <div class="flex space-around vertical flex2" style="padding-right: 200px;">
+      <h1>Discover your next favorite thing</h1>
+      <p>Product Hunt surfaces the best new products, every day. It's a place for product-loving enthusiasts to share and geek out about the latest mobile apps, websites, hardware projects, and tech creations.</p>
     </div>
-    <div>
-      <div class="trip">
-        <div class="flex space-between vertical">
-        <hr>
-          <div class="trip-navigation">
-            <nav class="flex horizontal space-around">
-              <a onclick="changeTab(this, 'modify-trip-scenario')" class="selected">Scenario</a>
-              <a onclick="changeTab(this, 'modify-trip-transport')">Transport</a>
-              <a onclick="changeTab(this, 'modify-trip-checklist')">Checklist</a>
-              <a onclick="changeTab(this, 'modify-trip-services')">Services</a>
-              <a onclick="changeTab(this, 'modify-trip-tasks')">Tasks</a>
-              <a onclick="changeTab(this, 'modify-trip-travelers')">Travelers</a>
-            </nav>
+    <div class="flex flex1">
+      <img style="width:240px;height:221px;margin: 20px;" src="https://s3.producthunt.com/static/kitty_265x244%402x.png" />
+    </div>
+  </div>
+</div>
+<?php endif; ?>
 
-          </div>
+<div class="container flex space-between horizontal mt30">
+  <div class="flex flex-start vertical flex2">
+    <div class="block">
+      <div class="flex space-between vertical">
+        <div class="trip-navigation">
+          <nav class="flex horizontal space-around">
+            <a onclick="changeTab(this, 'modify-trip-scenario')" class="selected">Scenario</a>
+            <a onclick="changeTab(this, 'modify-trip-transport')">Transport</a>
+          </nav>
+
         </div>
       </div>
-
+    </div>
   </div>
+  <div class="flex flex-start vertical flex1">
+    <div class="block">
+      <h1>Some news</h1>
+      <hr>
+      <p>Some text session_status() == PHP_SESSION_NONE </p>
+    </div>
+    <div class="block">
+      <div class="flex space-between vertical">
+        <div class="trip-navigation">
+          <nav class="flex horizontal space-around">
+            <a onclick="changeTab(this, 'modify-trip-scenario')" class="selected">Scenario</a>
+            <a onclick="changeTab(this, 'modify-trip-transport')">Transport</a>
+          </nav>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
 
 
 
--- project/pages/parts/header.php
+++ project/pages/parts/header.php
@@ -1,7 +1,15 @@
 <html>
-  <head>
-    <link rel="stylesheet" type="text/css" href="../dist/css/base.css" />
-    <!-- Add icon library -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  </head>
-  <body>
+
+<head>
+  <link rel="stylesheet" type="text/css" href="../dist/css/base.css" />
+  <!-- Add icon library -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+  <div class="block">
+    <div class="container flex space-between horizontal">
+      <a href="trips.html">Back to Trips</a>
+      <a href="trips.html">Remove this trip</a>
+    </div>
+  </div>
