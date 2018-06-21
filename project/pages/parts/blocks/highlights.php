4087e31a2f45e3c294c59cd69c08b562d8d09b49
timadevelop <timadevelop@gmail.com>
2018-06-21 04:44:18 +0300

    now i like how uls of items looks

--- project/pages/parts/blocks/highlights.php
+++ project/pages/parts/blocks/highlights.php
@@ -1,11 +1,13 @@
-    <div class="block">
-      <h4>Last month highlights</h4>
-      <hr>
-      <ul>
-        <li>
-          <p>Hello</p>
-        </li>
-        <li><p>it's me</p>
-      <ul>
-
+<div class="block">
+  <h4>Last month highlights</h4>
+  <hr>
+  <ul class="items">
+    <?php include('parts/item.php') ?>
+    <?php include('parts/item.php') ?>
+    <?php include('parts/item.php') ?>
+    <?php include('parts/item.php') ?>
+    <div class="flex space-around horizontal">
+      <a href="">Show more</a>
     </div>
+  </ul>
+</div>
---
+++ project/pages/parts/item.php
@@ -0,0 +1,18 @@
+
+    <li class="flex horizontal flex1">
+      <img src="https://cdn.svgporn.com/logos/nomad.svg" />
+      <div class="flex space-between vertical flex3">
+        <h4>Some title</h4>
+        <span>Some text is here for some time and this is great</span>
+        <div class="actions flex space-between horizontal">
+          <a href="" class="author">
+            <img src="https://cdn.svgporn.com/logos/gatsby.svg" />
+          </a>
+          <div class="flex flex-end horizontal">
+            <a class="button" href="login.html">more</a>
+            <a class="button" href="login.html">subscribe</a>
+          </div>
+        </div>
+      </div>
+
+    </li>
