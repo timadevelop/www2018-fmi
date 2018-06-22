5f9384928e1aec250977b8337b5e07886739acc3
timadevelop <timadevelop@gmail.com>
2018-06-22 21:11:59 +0300

    news

--- project/pages/parts/blocks/news.php
+++ project/pages/parts/blocks/news.php
@@ -1,5 +1,9 @@
-    <div class="block">
+    <div class="block news">
       <h4>Some news</h4>
       <hr>
-      <p>here is a list of last news.</p>
+      <div class="flex vertical">
+        <a href="">The must-see countries that are much cheaper to visit post-Brexit</a>
+        <a href="">120,000 sign petition to save one of Europe's last unspoiled corners</a>
+        <a href="">Travel World Cup: Switzerland battles Brazil for supremacy, but it's all over for Serbia</a>
+      </div>
     </div>
