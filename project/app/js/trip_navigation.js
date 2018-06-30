5cecdeecdb8dcde26d66a752a6b746f7ae577126
timadevelop <timadevelop@gmail.com>
2018-06-30 19:43:51 +0300

    filter

--- project/app/js/trip_navigation.js
+++ project/app/js/trip_navigation.js
@@ -13,3 +13,16 @@ window.changeTab = (el, tabName) => {
   }
   document.getElementById(tabName).classList.remove('hidden');
 }
+
+window.filter = function(value) {
+  var filter = value.toUpperCase();
+  var lis = document.getElementsByClassName('transport_item');
+  for (var i = 0; i < lis.length; i++) {
+    var name = lis[i].getElementsByClassName('type')[0].innerHTML;
+    if (name.toUpperCase().indexOf(filter) == 0)
+      lis[i].style.display = 'list-item';
+    else
+      lis[i].style.display = 'none';
+  }
+
+}
